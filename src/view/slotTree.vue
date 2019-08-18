ca7c2859d0f3cc394768d5c58e997f58207ab2e4
panxinhua <1109588767@qq.com>
2019-08-18 23:27:38 +0800

    first commit

--- src/view/slotTree.vue
+++ src/view/slotTree.vue
@@ -2,21 +2,21 @@
  @component elementUI@2.7.2 Tree 组件二次开发
  -->
 <template>
-	<div v-loading="isLoading" class="comp-tree">
-		<el-button class="comp-tr-top" 
-			type="primary" 
-			size="small" 
-			@click="handleAddTop">添加顶级节点</el-button>
+	<div v-loading="isLoading" class="comp-tree" @click="hiddenMenu">
 		<!-- tree -->
 		<el-tree ref="SlotTree"
 			:data="setTree"
-			:props="defaultProps"
-			:expand-on-click-node="false"
+			:props="defaultProps"		
 			highlight-current
+      @node-drop="handleDrop"
+      @node-contextmenu='rihgtClick'
+      @node-click="handleNodeClick"
+      draggable
 			:node-key="NODE_KEY">
 				<div class="comp-tr-node" slot-scope="{ node, data }">
 					<!-- 编辑状态 -->
 					<template v-if="node.isEdit">
+            <i class="el-icon-folder"></i>
 						<el-input v-model="data.name" 
 							autofocus
 							size="mini"
@@ -29,35 +29,40 @@
 					<template v-else>
 						<!-- 名称： 新增节点增加class（is-new） -->
 						<span :class="[data[NODE_KEY] < NODE_ID_START ? 'is-new' : '', 'comp-tr-node--name']">
-							{{ node.label }}
-						</span>
-						
-						<!-- 按钮 -->
-						<span class="comp-tr-node--btns">
-							<!-- 新增 -->
-							<el-button icon="el-icon-plus" 
-								size="mini"
-								circle 
-								type="primary"
-								@click="handleAdd(node, data)"></el-button>
-
-							<!-- 编辑 -->
-							<el-button icon="el-icon-edit" 
-								size="mini"
-								circle 
-								type="info"
-								@click="handleEdit(node, data)"></el-button>
-
-							<!-- 删除 -->
-							<el-button icon="el-icon-delete" 
-								size="mini"
-								circle 
-								type="danger"
-								@click="handleDelete(node, data)"></el-button>
+							<i class="el-icon-folder"></i>
+              {{ node.label }}
 						</span>
 					</template>
 				</div>
 			</el-tree>
+      <!-- 右键产生的选项 -->
+      <div v-show="menuVisible" id="menu">
+        <el-menu
+          id = "rightClickMenu"
+          class="el-menu-vertical"
+          @select="handleRightSelect">
+          <el-menu-item index="1" class="menuItem">
+            <i class="el-icon-edit"></i>
+            <span slot="title">重命名</span>
+          </el-menu-item>
+          <el-menu-item index="2" class="menuItem">
+            <i class="el-icon-folder-add"></i>
+            <span slot="title">新建文件夹</span>
+          </el-menu-item>
+          <el-menu-item index="3" class="menuItem">
+            <i class="el-icon-delete"></i>
+            <span slot="title">删除</span>
+          </el-menu-item>
+          <el-menu-item index="4" class="menuItem">
+            <i class="el-icon-top"></i>
+            <span slot="title">升级排序</span>
+          </el-menu-item>
+          <el-menu-item index="5" class="menuItem">
+            <i class="el-icon-bottom"></i>
+            <span slot="title">降级排序</span>
+          </el-menu-item>
+        </el-menu>
+      </div>
 	</div>
 </template>
 
@@ -82,7 +87,8 @@ export default{
 				name: '新增节点',
 				pid: 0,
 				children: []
-			},
+      },
+      menuVisible: false,
 		}
 	},
 	created(){
@@ -94,7 +100,7 @@ export default{
 			console.log(_node, _data)
 			// 判断是否存在子节点
 			if(_data.children && _data.children.length !== 0){
-				this.$message.error("此节点有子级，不可删除！")
+				this.$message.error("此文件夹里含有其他文件夹，不能删除！")
 				return false;
 			}else{
 				// 删除操作
@@ -167,11 +173,72 @@ export default{
 				_node.expanded = true
 			}
 		},
-		handleAddTop(){// 添加顶部节点
-			let obj = JSON.parse(JSON.stringify(this.initParam));// copy参数
-			obj[this.NODE_KEY] = --this.startId;// 节点id：逐次递减id
-			this.setTree.push(obj)
-		}
+    handleDrop(draggingNode, dropNode, dropType, ev) {
+      console.log('tree drop: ', dropNode.label, dropType);
+    },
+    rihgtClick(event, object, value, element) {
+      if (this.objectID !== object.id) {
+        this.objectID = object.id;
+        this.menuVisible = true;
+        this.DATA = object;
+        this.NODE = value;
+      } else {
+        this.menuVisible = !this.menuVisible;
+      }
+      document.addEventListener('click',(e)=>{
+        this.menuVisible = false;
+      })
+      let menu = document.querySelector("#rightClickMenu");
+      /* 菜单定位基于鼠标点击位置 */
+      menu.style.left = event.clientX + 20 + "px";
+      menu.style.top = event.clientY - 30 + "px";
+      menu.style.position = "absolute"; // 为新创建的DIV指定绝对定位
+      menu.style.width = 160 + "px";
+    },
+    handleRightSelect(key) {
+      console.log(key);
+      if (key == 1) {
+        // this.NodeAdd(this.NODE, this.DATA);
+        this.handleEdit(this.NODE, this.DATA)
+        this.menuVisible = false;
+      } else if (key == 2) {
+        this.handleAdd(this.NODE, this.DATA)
+        this.menuVisible = false;
+      } else if (key == 3) {
+        this.handleDelete(this.NODE, this.DATA)
+        this.menuVisible = false;
+      } else if(key == 4){
+        console.log('4')
+        this.menuVisible = false;
+      }
+    },
+    NodeAdd(n, d){//新增节点
+			console.log(n, d)
+			//判断层级
+			if(n.level >= 3){
+				this.$message.error("最多只支持三级！")
+				return false;
+			}
+			//新增数据
+			d.children.push({
+				id: ++this.maxexpandId,
+				name: '新增节点',
+				pid: d.id,
+				children: []
+			})
+			//同时展开节点
+			if(!n.expanded){
+				n.expanded = true
+			}
+    },
+    handleNodeClick(node,data){
+      this.menuVisible = false
+    },
+    hiddenMenu(event) {
+      document.addEventListener('click',function(){
+        this.menuVisible = false
+      })
+    }
 	}
 }
 </script>
